21115a468ce82d9de63426d7c0637ccfdc982d00
a1013457richar <r11722013@g.ntu.edu.tw>
2024-02-25 16:49:29 +0800

    Initial commit

---
+++ auth.ts
@@ -0,0 +1,64 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { connectToMongoDB } from "./lib/db";
+import User, { IUser } from "./models/userModel";
+
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async session({ session }) {
+      try {
+        await connectToMongoDB();
+        if (session.user) {
+          const user = await User.findOne({ email: session.user.email });
+          if (user) {
+            session.user._id = user._id;
+            return session;
+          } else {
+            throw new Error("User not found");
+          }
+        } else {
+          throw new Error("Invalid session");
+        }
+      } catch (error) {
+        console.log(error);
+        throw new Error("Invalid session");
+      }
+    },
+    async signIn({ account, profile }) {
+      if (account?.provider === "github") {
+        await connectToMongoDB();
+
+        try {
+          const user = await User.findOne({ email: profile?.email });
+
+          // signup the user if not found
+          if (!user) {
+            const newUser = await User.create({
+              username: profile?.login,
+              email: profile?.email,
+              fullName:
+                profile?.name || Math.random().toString(36).substring(7),
+              avatar: profile?.avatar_url,
+            });
+
+            await newUser.save();
+          }
+          return true; // indicate successful sign-in
+        } catch (error) {
+          console.log(error);
+          return false; // indicate failed sign-in
+        }
+      }
+
+      return false; // indicate failed sign-in
+    },
+  },
+});
